d94eeb84be07ebc101e43f3c58f87fa16aac90e1
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-01-26 10:26:55 +0100

    Fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Posts from './components/Posts';
 import MyProfilePic from './components/MyProfilePic';
 
-export const revalidate = 86400;
+export const revalidate = 7200;
 
 export default function Home() {
   return (
